9e8585177df3308c7ec2c47e9ac55a196e1ed32d
John Stile <johns@msli.com>
2020-05-12 10:23:20 -0700

    Adding a few message queue types

--- message_queue.py
+++ message_queue.py
@@ -19,13 +19,35 @@ class QueueMessage(object):
     def handle(self, receiver):
         raise NotImplementedError
 
+
+class QuitMessage(QueueMessage):
+    """Worker tells Boss it is ending"""
+    def __init__(self, sender_id, this_exit_code):
+        self.worker_id = sender_id 
+        self.this_exit_code = this_exit_code
+
+    def handle(self, receiver):
+        receiver.on_quit(self.sender_id, self.this_exit_code)
+
+
+class LogMessage(QueueMessage):
+    """Worker tells Boss to write to it's log"""
+    def __init__(self, sender_id, log_level, msg):
+        self.sender_id = sender_id
+        self.log_level = log_level
+        self.msg = msg
+
+    def handle(self, receiver):
+        receiver.on_log(self.sender_id, self.log_level, self.msg)
+
+
 class StatusMessage(QueueMessage):
     """Worker<->Boss status messages"""
-    def __init__(self, worker_id, data):
-        self.worker_id = worker_id
+    def __init__(self, sender_id, data):
+        self.sender_id = sender_id
         self.data = data
 
     def handle(self, receiver):
         # Promissed method implemented in receiver
-        receiver.on_status(self.worker_id, self.data)
+        receiver.on_status(self.sender_id, self.data)
 
